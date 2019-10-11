ebc047b64ef30a5306fa62333440975d555a7ba3
jonahgolden <jonahwgolden@gmail.com>
2019-10-11 13:03:25 -0700

    trying to get this to work

--- src/main/java/io/swagger/Main.java
+++ src/main/java/io/swagger/Main.java
@@ -1,14 +1,24 @@
 package io.swagger;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Arrays;
 
-@RestController
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class Main {
 
-  @RequestMapping("/")
-  public String index() {
-    return "Greetings from Spring Boot!";
+  public static void main(String[] args) {
+    ApplicationContext ctx = SpringApplication.run(Main.class, args);
+
+    System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+    String[] beanNames = ctx.getBeanDefinitionNames();
+    Arrays.sort(beanNames);
+    for (String beanName : beanNames) {
+      System.out.println(beanName);
+    }
   }
 
 }
