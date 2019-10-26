e2c5f4986d45fcbcc7c1ae7338e8a063a48168ac
jonahgolden <jonahwgolden@gmail.com>
2019-10-25 17:55:43 -0700

    mongo

--- src/main/java/io/swagger/Swagger2SpringBoot.java
+++ src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -3,18 +3,20 @@ package io.swagger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableSwagger2
 @ComponentScan(basePackages = { "io.swagger", "io.swagger.api" , "io.swagger.configuration"})
-public class Swagger2SpringBoot extends SpringBootServletInitializer {
+public class Swagger2SpringBoot implements CommandLineRunner {
 
-    //@Override
+    @Override
     public void run(String... arg0) throws Exception {
         if (arg0.length > 0 && arg0[0].equals("exitcode")) {
             throw new ExitException();
