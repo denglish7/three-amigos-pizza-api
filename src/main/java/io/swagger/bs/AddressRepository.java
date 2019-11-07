f96a12023f9c0d893bce823a5f786f09870cfbb1
jonahgolden <jonahwgolden@gmail.com>
2019-11-07 12:09:13 -0800

    commenting for error

---
+++ src/main/java/io/swagger/bs/AddressRepository.java
@@ -0,0 +1,8 @@
+//package io.swagger.repositories;
+//
+//import io.swagger.model.customer.Address;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//public interface AddressRepository extends MongoRepository<Address, String> {
+//
+//}
